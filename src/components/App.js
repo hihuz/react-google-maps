2e900428812c1aeb9b5f445a2a45d421f95f0043
Sylvain Boulade <bopulie@gmail.com>
2017-05-11 10:23:06 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/main.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        React App !
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = env => ({
+  context: __dirname,
+  entry: "./src/index.js",
+  devtool: env.prod ? "cheap-module-source-map" : "eval",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+    publicPath: ""
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: [".js", ".json"]
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: "json-loader"
+      },
+      {
+        include: path.resolve(__dirname, "src"),
+        test: /\.js$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader"]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html.ejs"
+    }),
+    new ExtractTextPlugin("main.css")
+  ]
+});
